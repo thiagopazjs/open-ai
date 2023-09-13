e49029a137696deb9ac4dbd00f2dbe80a46af8f6
Thiago Paz <tpazdev@gmail.com>
2023-09-13 10:46:42 -0300

    feat: add shadcn and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { Github } from 'lucide-react'
+import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
 
-import './App.css'
 
 export function App() {
- 
+
 
   return (
-    <h1>hello Worlod</h1>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className='text-xl font-bold '>Upload-ia</h1>
+
+
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>
+            Desenvolvido NLW
+          </span>
+
+          <Separator orientation='vertical' className='h-6'/>
+
+          <Button variant="outline">
+            <Github className='w-4 h-4 mr-2'/>
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
